d54087060c44cdba987cd7cc106bb2ba9271bed3
Szymon <seltinxx@gmail.com>
2021-05-16 18:07:26 +0200

    zad

--- main.py
+++ main.py
@@ -4,29 +4,47 @@ import matplotlib.pyplot as plt
 #Zadanie 1
 
 
-# a = pd.ExcelFile('imiona.xlsx')
-# b = pd.read_excel(a, header=0)
-# print(b)
+a = pd.ExcelFile('imiona.xlsx')
+b = pd.read_excel(a, header=0)
+print(b)
 #
 # c = b.groupby(['Rok'])['Liczba'].sum()
 # print(c)
 # c.plot()
 # plt.show()
+# Nie mam pojęcia jak zrobić żeby nie było połowy roku
 
 #Zadanie 2
 
-a = pd.ExcelFile('imiona.xlsx')
-b = pd.read_excel(a, header=0)
-print(b)
-# df = pd.DataFrame(a)
-# print(df)
+# a = pd.ExcelFile('imiona.xlsx')
+# b = pd.read_excel(a, header=0)
+# print(b)
+#
+#
+# grupak = b[(b['Plec'] == 'K')]['Liczba'].sum()
+# grupam = b[(b['Plec'] == 'M')]['Liczba'].sum()
+# title = ['Kobiety', 'Mezczyzni']
+# grupa = [grupak, grupam]
+# plt.bar(title, grupa, color='blue')
+# plt.ylabel('Ilosc narodzin')
+# plt.show()
 
-grupa = b.groupby(['Plec']).agg({'Plec':['sum']})
-print(grupa)
-wykres = grupa.plot.bar()
-wykres.set_xlabel('Plec')
-wykres.set_ylabel('Ilosc')
-wykres.legend()
-plt.title("Urodzenie chlopcy i dziewczeta")
-plt.xticks(rotation=0)
+#Zadanie 3
+
+# a = pd.ExcelFile('imiona.xlsx')
+# b = pd.read_excel(a, header=0)
+# print(b)
+#
+#
+# zad = b[(b['Rok'] >= 2012)].groupby(['Plec']).agg({'Liczba': {'sum'}})
+# title = ['Kobieta', 'Mezczyzna']
+# wykres = zad.plot.pie(figsize=(5, 5), autopct='%.2f %%', subplots=True, labels=title, startangle=90, shadow=True, wedgeprops={'edgecolor': 'white'}, colors=['y', 'r'])
+# plt.title('Liczba urodzen w ciagu 5 lat')
+# plt.show()
+
+#Zadanie 4
+
+a = pd.read_csv('zamowienia.csv',header=0, sep=';', decimal='.')
+b = a.groupby(['Sprzedawca'])[['idZamowienia']].count().plot(kind='bar', color='pink', legend=None, title='Ilosc zamowien zlozonych przez sprzedawcow')
+plt.ylabel('Ilosc zlozonych zamowien')
 plt.show()
